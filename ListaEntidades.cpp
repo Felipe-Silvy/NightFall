27b4ac9b725f1123c48ef79c30fa7716ba4e7a4d
Vitor Rossi <vitor.2025@alunos.utfpr.edu.br>
2025-11-04 22:15:40 -0300

    Funcao incluir ListaEntidades

--- ListaEntidades.cpp
+++ ListaEntidades.cpp
@@ -10,6 +10,9 @@ Nightfall::Listas::ListaEntidades::~ListaEntidades()
 
 void Nightfall::Listas::ListaEntidades::incluir(Entidades::Entidade* pE)
 {
+	if (pE != nullptr)
+		LEs.incluir(pE);
+	//inclui a entidade pE na Lista parametrizada com Entidade
 }
 
 void Nightfall::Listas::ListaEntidades::percorrer()
